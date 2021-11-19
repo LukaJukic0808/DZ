bd6c79c24ff1bf274ae8596d256e194587279751
LukaJukic0808 <92713534+LukaJukic0808@users.noreply.github.com>
2021-11-19 21:00:15 +0100

    Add files via upload
    Added DZ2!

---
+++ DZ2/DZ1_Logic/WeeklyForecast.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2_Logic
+{
+    public class WeeklyForecast
+    {
+        private DailyForecast[] forecasts;
+
+
+        public WeeklyForecast(DailyForecast[] forecasts)
+        {
+            this.forecasts = forecasts;
+        }
+
+        public DailyForecast this[int forecastIndex]
+        {
+            get { return forecasts[forecastIndex]; }
+        }
+
+        public string GetAsString()
+        {
+            string all = "";
+            for(int i = 0; i < forecasts.Length; i++)
+            {
+                 all += forecasts[i].GetAsString()+'\n';
+            }
+
+            return all;
+        }
+
+        public double GetMaxTemperature()
+        {
+            DailyForecast max = forecasts[0];
+            for(int i = 0; i < forecasts.Length; i++)
+            {
+                if (forecasts[i] > max)
+                    max = forecasts[i];
+            }
+
+            return max.GetTemperature();
+        }
+    }
+}
